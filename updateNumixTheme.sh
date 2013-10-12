13f3ff10d3e106aa8ce20d9750bf038cc6ad4dea
Anton Savenko <jinn.mv@gmail.com>
2013-10-12 13:08:40 +0300

    line endings are optimized

--- updateNumixTheme.sh
+++ updateNumixTheme.sh
@@ -10,34 +10,29 @@ TARGER_COLOR="2F5FA3"
 
 # Debug info
 if [ $DEBUG -eq 1 ] ; then
-    echo "*** Debug information ***"
-    echo "  Source theme path: $SOURCE_PATH"
-    echo "  Theme name:        $THEME_NAME"
-    echo ""
-    echo "  Target path:       $TARGET_PATH"
-    echo "  Theme modificator: $THEME_MODIFICATOR"
-    echo ""
+    echo    "*** Debug information ***"
+    echo    "  Source theme path: $SOURCE_PATH"
+    echo -e "  Theme name:        $THEME_NAME\n"
+    echo    "  Target path:       $TARGET_PATH"
+    echo -e "  Theme modificator: $THEME_MODIFICATOR\n"
     echo "  Replace color:    $SOURCE_COLOR"
-    echo "  Target color:     $TARGER_COLOR"
-    echo ""
+    echo -e "  Target color:     $TARGER_COLOR\n"
 fi
 
 # Check of source path is exist
 if [ $DEBUG -eq 1 ] ; then
-    echo "DEBUG: Checking of source path $SOURCE_PATH"
-    echo ""
+    echo -e "DEBUG: Checking of source path $SOURCE_PATH\n"
 fi
 
 if [ ! -d $SOURCE_PATH ] ; then
     echo "Error: Source directory $SOURCE_PATH doesn't exist";
-    echo ""
     exit 1;
 fi
 
 # Copy newest files
 if [ $DEBUG -eq 1 ] ; then
-    echo "DEBUG: Copying new files\n$SOURCE_PATH -> $TARGET_PATH"
-    echo ""
+    echo -e "DEBUG: Copying new files"
+    echo -e "       $SOURCE_PATH -> $TARGET_PATH\n"
     rsync -auv $SOURCE_PATH/ $TARGET_PATH/;
 else
     rsync -au $SOURCE_PATH/ $TARGET_PATH/;
@@ -45,17 +40,14 @@ fi
 
 # Replace theme name
 if [ $DEBUG -eq 1 ] ; then
-    echo ""
-    echo "DEBUG: Altering theme name '$THEME_NAME' -> '$THEME_NAME-$THEME_MODIFICATOR'"
+    echo -e "\nDEBUG: Altering theme name '$THEME_NAME' -> '$THEME_NAME-$THEME_MODIFICATOR'"
     echo "       in file $TARGET_PATH/index.theme"
 fi
 sed -i "s/=$THEME_NAME$/=$THEME_NAME-$THEME_MODIFICATOR/g" $TARGET_PATH/index.theme
 
 # Replace colors
 if [ $DEBUG -eq 1 ] ; then
-    echo ""
-    echo "DEBUG: Replacing color #$SOURCE_COLOR -> #$TARGER_COLOR"
-    echo ""
+    echo -e "\nDEBUG: Replacing color #$SOURCE_COLOR -> #$TARGER_COLOR\n"
 fi
 grep -rl "$SOURCE_COLOR" $TARGET_PATH/* | xargs sed -i "s/$SOURCE_COLOR/$TARGER_COLOR/g"
 
